52c46c1dd600d4ec70938c3c4bb0b7314cb96334
y-nagai0725 <y.nagai0725@gmail.com>
2024-07-03 03:14:22 +0900

    バーガーアイコンクリックでメニュー展開まで

--- js/top.js
+++ js/top.js
@@ -1,8 +1,31 @@
 //TOPページ用JS
 document.addEventListener('DOMContentLoaded', function () {
 
+  //ページ全体
+  const pageWrapper = document.getElementById("wrapper");
+
+  //グローバルナビゲーションメニュー
+  const gnavMenu = document.getElementById("gnav__menu");
+
+  //グローバルナビゲーションボタン
+  const gnavButton = document.getElementById("gnav__button");
+
   //GSAPメディアクエリ
   const mm = gsap.matchMedia();
+
+  gnavButton.addEventListener("click", function () {
+    this.classList.toggle("js-opened");
+    gnavMenu.classList.toggle("js-opened");
+    pageWrapper.classList.toggle("js-blur");
+  });
+
+
+
+
+
+
+
+
 
   //ScrollTrigger使用
   gsap.registerPlugin(ScrollTrigger);
